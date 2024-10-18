c5da497fb0738888c757cba6d73afe8bdfa9f87e
sch990428 <990428sch@naver.com>
2024-10-18 17:45:23 +0900

    리소스 매니저
    리소스 매니저

---
+++ Spider Jellitaire/Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,33 @@
+using Mono.Cecil;
+using UnityEngine;
+
+public class ResourceManager : Singleton<ResourceManager>
+{
+	public T Load<T>(string path) where T : Object
+	{
+		T resource = Resources.Load<T>(path);
+		if (resource == null)
+		{
+			Debug.Log($"{path}에 해당 리소스가 존재하지 않습니다");
+		}
+		return resource;
+	}
+
+	public GameObject Instantiate(string path, Transform parent)
+	{
+		GameObject original = Load<GameObject>(path);
+
+		if (original == null){ return null; }
+
+		GameObject go = Instantiate(original, parent);
+		go.name = original.name;
+
+		return go;
+	}
+
+	public void Destroy(GameObject go, float t = 0f)
+	{
+		if (go == null){ return; }
+		UnityEngine.Object.Destroy(go, t);
+	}
+}

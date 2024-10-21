622fa6695cbfefa02736ac5117121d47dec2c7b0
sch990428 <990428sch@naver.com>
2024-10-21 15:23:56 +0900

    퍼즐 맵 디자인
    퍼즐 맵 디자인

--- Spider Jellitaire/Assets/Scripts/Managers/GameManager.cs
+++ Spider Jellitaire/Assets/Scripts/Managers/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : Singleton<GameManager>
 		for (int i = 0; i < 7; i++)
 		{
 			GameObject go = ResourceManager.Instance.Instantiate("Prefabs/GameEntity/Jelly");
-			go.transform.position = new Vector3(i - 3, 0, 6);
+			go.transform.position = new Vector3(i - 3, 0, 7.5f);
 			Jelly j = go.GetComponent<Jelly>();
 			j.Number = Random.Range(1, 6);
 			j.ChangeType(Define.JellyType.Green);

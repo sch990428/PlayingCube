2cc4da543d668f4437f4bda9f83b74d50ac1b99b
sch990428 <990428sch@naver.com>
2024-10-21 16:34:17 +0900

    새로운 줄 추가

--- Spider Jellitaire/Assets/Scripts/Managers/GameManager.cs
+++ Spider Jellitaire/Assets/Scripts/Managers/GameManager.cs
@@ -6,20 +6,59 @@ using UnityEngine.InputSystem;
 
 public class GameManager : Singleton<GameManager>
 {
+	[SerializeField]
+	private GameObject LineRoots;
+
 	public bool anyJellyMoving;
 
 	protected override void Awake()
 	{
 		base.Awake();
+		AddJellyLine();
+		AddJellyLine();
+	}
 
+	private void AddJellyLine()
+	{
 		for (int i = 0; i < 7; i++)
 		{
-			GameObject go = ResourceManager.Instance.Instantiate("Prefabs/GameEntity/Jelly");
-			go.transform.position = new Vector3(i - 3, 0, 7.5f);
-			Jelly j = go.GetComponent<Jelly>();
+			Transform line = LineRoots.transform.GetChild(i);
+
+			GameObject newJelly = ResourceManager.Instance.Instantiate("Prefabs/GameEntity/Jelly");
+			Jelly j = newJelly.GetComponent<Jelly>();
 			j.Number = Random.Range(1, 6);
 			j.ChangeType(Define.JellyType.Green);
-			go.name += i;
+
+			if (line.childCount == 0)
+			{
+				newJelly.transform.SetParent(line.transform);
+				newJelly.transform.position = new Vector3(i - 3, 0, 7.5f);
+			}
+			else
+			{
+				// 이미 하위에 젤리가 있다는 뜻이므로 링크드리스트처럼 순회
+				Jelly current = line.GetChild(0).GetComponent<Jelly>();
+				int height = 1;
+				while (current.Child != null)
+				{
+					current = current.Child;
+					height++;
+				}
+
+				current.Child = j;
+				j.Parent = current;
+				newJelly.transform.SetParent(current.transform);
+				newJelly.transform.position = new Vector3(i - 3, 0, 7.5f - height);
+			}
+			
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			AddJellyLine();
 		}
 	}
 }

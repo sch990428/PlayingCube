21edfc693c700eb12923fdb478833f26127a79b1
sch990428 <990428sch@naver.com>
2024-10-23 16:02:09 +0900

    초기 파일
    초기 파일

--- Spider Jellitaire/Assets/Scripts/Utils/Define.cs
+++ Spider Jellitaire/Assets/Scripts/Utils/Define.cs
@@ -13,11 +13,11 @@ public static class Define
 
 	public static void InitJellyColor()
 	{
-		JellyColor.Add(JellyType.Unknown, new Color32(46, 46, 46, 170));
-		JellyColor.Add(JellyType.Blue, new Color32(61, 102, 149, 170));
-		JellyColor.Add(JellyType.Red, new Color32(173, 69, 61, 170));
-		JellyColor.Add(JellyType.Green, new Color32(61, 149, 62, 170));
-		JellyColor.Add(JellyType.Yellow, new Color32(158, 159, 97, 170));
+		JellyColor.Add(JellyType.Unknown, new Color32(46, 46, 46, 230));
+		JellyColor.Add(JellyType.Blue, new Color32(61, 102, 149, 230));
+		JellyColor.Add(JellyType.Red, new Color32(173, 69, 61, 230));
+		JellyColor.Add(JellyType.Green, new Color32(61, 149, 62, 230));
+		JellyColor.Add(JellyType.Yellow, new Color32(158, 159, 97, 230));
 	}
 
 	public enum JellyType

c6152ddd26c93d5d34e921da1cf29aacbceb3381
sch990428 <990428sch@naver.com>
2024-10-24 13:12:08 +0900

    UI 전환 함수 일부 수정 및 주석추가
    UI 전환 함수 일부 수정 및 주석추가

--- Jelitaire/Assets/Scripts/Controllers/UIController.cs
+++ Jelitaire/Assets/Scripts/Controllers/UIController.cs
@@ -15,7 +15,7 @@ public class UIController : MonoBehaviour
 	[SerializeField]
 	private Transform GameUI;
 
-	private float switchTerm = 0.4f;
+	private float switchTerm = 0.5f; // UI을 전환하는 간격
 
 	public void OnPlayButtonClicked()
 	{
@@ -29,20 +29,22 @@ public class UIController : MonoBehaviour
 
 	private IEnumerator SwitchToGameUI()
 	{
+		// 로비 UI에서 게임 UI로 전환
 		StartCoroutine(FadeOutUIGroup(StaticLobbyUI));
 		StartCoroutine(FadeOutUIGroup(ModeSelectUI));
 		StartCoroutine(SlideDownBackgrounds());
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(switchTerm);
 
 		StartCoroutine(FadeInUIGroup(GameUI));
 	}
 
 	private IEnumerator SwitchToLobbyUI()
 	{
+		// 게임 UI에서 로비 UI로 전환
 		StartCoroutine(FadeOutUIGroup(GameUI));
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(switchTerm);
 
 		StartCoroutine(FadeInUIGroup(StaticLobbyUI));
 		StartCoroutine(FadeInUIGroup(ModeSelectUI));
@@ -51,24 +53,27 @@ public class UIController : MonoBehaviour
 
 	private IEnumerator FadeOutUIGroup(Transform t)
 	{
+		// 서서히 사라지는 Fade Out효과
 		Animator animator = t.GetComponent<Animator>();
 		animator.SetTrigger("FadeOut");
-		yield return new WaitForSeconds(switchTerm);
+		yield return new WaitForSeconds(0.5f);
 		t.gameObject.SetActive(false);
 		animator.ResetTrigger("FadeOut");
 	}
 
 	private IEnumerator FadeInUIGroup(Transform t)
 	{
+		// 서서히 나타나는 Fade In효과
 		t.gameObject.SetActive(true);
 		Animator animator = t.GetComponent<Animator>();
 		animator.SetTrigger("FadeIn");
-		yield return new WaitForSeconds(switchTerm);
+		yield return new WaitForSeconds(0.5f);
 		animator.ResetTrigger("FadeIn");
 	}
 
 	private IEnumerator SlideDownBackgrounds()
 	{
+		// 배경이 서서히 내려가는 Slide Down효과
 		Animator animator = Backgrounds.GetComponent<Animator>();
 		animator.SetTrigger("SlideDown");
 		yield return new WaitForSeconds(0.5f);
@@ -78,6 +83,7 @@ public class UIController : MonoBehaviour
 
 	private IEnumerator SlideUpBackgrounds()
 	{
+		// 배경이 서서히 올라오는 Slide Up효과
 		Backgrounds.gameObject.SetActive(true);
 		Animator animator = Backgrounds.GetComponent<Animator>();
 		animator.SetTrigger("SlideUp");

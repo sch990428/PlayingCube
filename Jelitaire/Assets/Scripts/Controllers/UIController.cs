1b8432e7aed836f44c4c2c648b595837786867e6
sch990428 <990428sch@naver.com>
2024-10-24 13:06:17 +0900

    부드러운 UI 전환을 위한 SwitchTerm 추가
    부드러운 UI 전환을 위한 SwitchTerm 추가

--- Jelitaire/Assets/Scripts/Controllers/UIController.cs
+++ Jelitaire/Assets/Scripts/Controllers/UIController.cs
@@ -12,20 +12,59 @@ public class UIController : MonoBehaviour
 	[SerializeField]
 	private Transform ModeSelectUI;
 
+	[SerializeField]
+	private Transform GameUI;
+
+	private float switchTerm = 0.4f;
+
 	public void OnPlayButtonClicked()
+	{
+		StartCoroutine(SwitchToGameUI());
+	}
+
+	public void OnPauseButtonClicked()
+	{
+		StartCoroutine(SwitchToLobbyUI());
+	}
+
+	private IEnumerator SwitchToGameUI()
 	{
 		StartCoroutine(FadeOutUIGroup(StaticLobbyUI));
 		StartCoroutine(FadeOutUIGroup(ModeSelectUI));
 		StartCoroutine(SlideDownBackgrounds());
+
+		yield return new WaitForSeconds(1f);
+
+		StartCoroutine(FadeInUIGroup(GameUI));
+	}
+
+	private IEnumerator SwitchToLobbyUI()
+	{
+		StartCoroutine(FadeOutUIGroup(GameUI));
+
+		yield return new WaitForSeconds(1f);
+
+		StartCoroutine(FadeInUIGroup(StaticLobbyUI));
+		StartCoroutine(FadeInUIGroup(ModeSelectUI));
+		StartCoroutine(SlideUpBackgrounds());
 	}
 
 	private IEnumerator FadeOutUIGroup(Transform t)
 	{
 		Animator animator = t.GetComponent<Animator>();
 		animator.SetTrigger("FadeOut");
-		yield return new WaitForSeconds(0.5f);
+		yield return new WaitForSeconds(switchTerm);
 		t.gameObject.SetActive(false);
 		animator.ResetTrigger("FadeOut");
+	}
+
+	private IEnumerator FadeInUIGroup(Transform t)
+	{
+		t.gameObject.SetActive(true);
+		Animator animator = t.GetComponent<Animator>();
+		animator.SetTrigger("FadeIn");
+		yield return new WaitForSeconds(switchTerm);
+		animator.ResetTrigger("FadeIn");
 	}
 
 	private IEnumerator SlideDownBackgrounds()
@@ -35,5 +74,14 @@ public class UIController : MonoBehaviour
 		yield return new WaitForSeconds(0.5f);
 		Backgrounds.gameObject.SetActive(false);
 		animator.ResetTrigger("SlideDown");
+	}
+
+	private IEnumerator SlideUpBackgrounds()
+	{
+		Backgrounds.gameObject.SetActive(true);
+		Animator animator = Backgrounds.GetComponent<Animator>();
+		animator.SetTrigger("SlideUp");
+		yield return new WaitForSeconds(0.5f);	
+		animator.ResetTrigger("SlideUp");
 	}
 }

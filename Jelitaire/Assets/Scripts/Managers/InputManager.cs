647585220565feb2116f560724fa8c3074351de2
sch990428 <990428sch@naver.com>
2024-10-26 01:36:58 +0900

    큐브 이동 로직 변경
    큐브 이동 로직 변경

--- Jelitaire/Assets/Scripts/Managers/InputManager.cs
+++ Jelitaire/Assets/Scripts/Managers/InputManager.cs
@@ -4,19 +4,22 @@ using UnityEngine.InputSystem;
 
 public class InputManager : Singleton<InputManager>
 {
+	// InputSystem 액션
 	private PlayerInput playerInput;
 	private InputAction touchPressAction;
 	private InputAction touchPositionAction;
 	private InputAction touchingAction;
 
-	private CubeController movingCube;
+	private CubeController movingCube; // 현재 움직이고 있는 큐브
 
+	// 큐브들이 구독할 Input 이벤트
 	public event Action<CubeController> OnTouchStart;
 	public event Action<Vector2> OnTouching;
-	public event Action<CubeController> OnTouchEnd;
+	public event Action OnTouchEnd;
 
 	protected override void Awake()
     {
+		// 액션 불러오기
 		playerInput = GetComponent<PlayerInput>();
 		touchPressAction = playerInput.actions["TouchPress"];
 		touchPositionAction = playerInput.actions["TouchPosition"];
@@ -25,6 +28,7 @@ public class InputManager : Singleton<InputManager>
 
 	private void OnEnable()
 	{
+		// 각 액션에 클릭 이벤트 함수 구독
 		touchPressAction.performed += TouchPressed;
 		touchPressAction.canceled += TouchReleased;
 		touchingAction.performed += Touching;
@@ -32,13 +36,16 @@ public class InputManager : Singleton<InputManager>
 
 	private void OnDisable()
 	{
+		// 각 액션에 클릭 이벤트 함수 해제
 		touchPressAction.performed -= TouchPressed;
 		touchPressAction.performed -= TouchReleased;
 		touchingAction.performed += Touching;
 	}
 
+	// 클릭 혹은 터치 최초 입력
 	private void TouchPressed(InputAction.CallbackContext context)
 	{
+		// 레이캐스팅으로 큐브를 선택
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(touchPositionAction.ReadValue<Vector2>());
 		Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 2f);
@@ -48,40 +55,46 @@ public class InputManager : Singleton<InputManager>
 			if (hit.collider.CompareTag("Cube") && OnTouchStart != null)
 			{
 				movingCube = hit.collider.GetComponent<CubeController>();
-				OnTouchStart.Invoke(movingCube);
+				OnTouchStart.Invoke(movingCube); // 구독한 큐브 객체들에게 모두 Broadcasting (식별을 위한 movingCube 인자)
 			}
 		}
 	}
 
+	// 클릭 혹은 터치 진행중
 	private void Touching(InputAction.CallbackContext context)
 	{
-		Vector2 touchPos = touchPositionAction.ReadValue<Vector2>();
-		Ray ray = Camera.main.ScreenPointToRay(touchPos);
-
-		Plane plane = new Plane(Vector3.forward, new Vector3(0, 0, 49.5f));
-
-		if (plane.Raycast(ray, out float enter))
+		if (movingCube != null)
 		{
-			Vector3 hitPoint = ray.GetPoint(enter);
+			// xy평면에 레이캐스팅하여 정확한 포인터의 위치를 알아냄
+			Vector2 touchPos = touchPositionAction.ReadValue<Vector2>();
+			Ray ray = Camera.main.ScreenPointToRay(touchPos);
 
-			if (OnTouching != null)
+			Plane plane = new Plane(Vector3.forward, new Vector3(0, 0, 49.5f));
+
+			if (plane.Raycast(ray, out float enter))
 			{
-				movingCube.GetComponent<Collider>().enabled = false;
+				Vector3 hitPoint = ray.GetPoint(enter);
 
-				hitPoint.x = Mathf.RoundToInt(hitPoint.x);
-				hitPoint.x = Mathf.Clamp(hitPoint.x, -2, 2);
-				
-				OnTouching.Invoke(hitPoint);
+				if (OnTouching != null)
+				{
+					movingCube.GetComponent<Collider>().enabled = false; // 드래그 도중 큐브끼리 충돌방지 (필요한가?)
+
+					hitPoint.x = Mathf.RoundToInt(hitPoint.x); // 소수점 반올림을 통해 좌표 변경이 1단위로 이루어지도록 함
+					hitPoint.x = Mathf.Clamp(hitPoint.x, -2, 2); // [-2, 2] 범위에서 움직임
+
+					OnTouching.Invoke(hitPoint); // 구독한 큐브 객체들에게 모두 Broadcasting (포인터 좌표 전달)
+				}
 			}
 		}
 	}
 
+	// 클릭 혹은 터치 종료
 	private void TouchReleased(InputAction.CallbackContext context)
 	{
 		if (OnTouchEnd != null && movingCube != null)
 		{
-			movingCube.GetComponent<Collider>().enabled = true;
-			OnTouchEnd.Invoke(movingCube);
+			movingCube.GetComponent<Collider>().enabled = true; // 콜라이더 되돌리기
+			OnTouchEnd.Invoke(); // 구독한 큐브 객체들에게 모두 Broadcasting (포인터 좌표 전달)
 			movingCube = null;
 		}
 	}

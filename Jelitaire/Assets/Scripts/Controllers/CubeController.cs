3263b991973eca8cfbac21263c61eb4fd81c2a79
sch990428 <990428sch@naver.com>
2024-10-26 01:36:35 +0900

    Cube 정보 저장 방식 변경
    큐브에 대한 정보를 Root만 저장하지 않고 전체 다 저장하도록 함

--- Jelitaire/Assets/Scripts/Controllers/CubeController.cs
+++ Jelitaire/Assets/Scripts/Controllers/CubeController.cs
@@ -3,59 +3,68 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class CubeController : MonoBehaviour
 {
+	// 큐브 개체 정보
 	[SerializeField]
-	private TMP_Text NumberText;
+	private TMP_Text NumberText; // 숫자를 표시할 TextMeshPro 오브젝트
 
-    public int Number;
-    public int Type;
-
-    public CubeController Parent; // 상위의 큐브 혹은 null(루트)
-	public CubeController Child; // 하위의 큐브 혹은 null
+    public int Number; // 숫자
+    public int Type; // 타입
 
 	private Animator animator;
-
 	private Renderer render;
+
+	// 큐브 계층 정보
+	public CubeController Parent; // 상위의 큐브 혹은 null(루트)
+	public CubeController Child; // 하위의 큐브 혹은 null
+
+	// 큐브 이동 관련 정보
+	private bool isMoving; 
+	private Vector3 prevPos;
 
 	private void Awake()
 	{
 		animator = GetComponent<Animator>();
 		render = GetComponent<Renderer>();
+
+		isMoving = false;
 	}
 
 	private void OnEnable()
 	{
+		// InputManager 구독
 		InputManager.Instance.OnTouchStart += TouchStart;
+		InputManager.Instance.OnTouching += Touching;
+		InputManager.Instance.OnTouchEnd += TouchReleased;
 	}
 
+	private void OnDisable()
+	{
+		// InputManager 구독 해제
+		InputManager.Instance.OnTouchStart -= TouchStart;
+		InputManager.Instance.OnTouching -= Touching;
+		InputManager.Instance.OnTouchEnd -= TouchReleased;
+	}
+
+	// 큐브 생성 혹은 아이템 사용으로 인한 큐브 정보 변경
 	public void SetValue(int number, int type)
 	{
 		Number = number;
 		Type = type;
 
 		NumberText.text = Number.ToString();
-		render.material.color = GameManager.Instance.CubeColors[Type];
+		render.material.color = GameManager.Instance.CubeColors[Type]; // 타입에 따른 색상 변경
 	}
 
-	private void OnCollisionEnter(Collision collision)
-	{
-		Rigidbody rigidbody = GetComponent<Rigidbody>();
-
-		if (!rigidbody.isKinematic)
-		{
-			// 충돌이 감지되면 큐브의 속도를 감소시켜 반동을 상쇄
-			Vector3 newVelocity = rigidbody.linearVelocity * 0.15f;
-			rigidbody.linearVelocity = newVelocity;
-		}
-	}
-
+	// 큐브 삭제 로직
 	public void DestroyCube()
 	{
 		if (Child != null)
 		{
-			Child.DestroyCube();
+			Child.DestroyCube(); // Child 재귀
 		}
 
 		animator.SetTrigger("Destroy");
@@ -63,38 +72,95 @@ public class CubeController : MonoBehaviour
 		ResourceManager.Instance.Destroy(gameObject, 0.35f);
 	}
 
+	// 해당 큐브가 선택되면 수행할 동작
 	private void TouchStart(CubeController target)
 	{
 		if (target == this)
 		{
-			transform.GetComponent<Rigidbody>().isKinematic = true;
-			InputManager.Instance.OnTouching += Touching;
-			InputManager.Instance.OnTouchEnd += TouchReleased;
+			prevPos = transform.position; // 이전 좌표 저장
+
+			// 기존 부모와의 연결 끊기
+			if (Parent != null)
+			{
+				Parent.Child = null;
+				Parent = null;
+			}
+
+			isMoving = true;
 		}
 	}
 
+	// 해당 큐브가 선택되는 도중에 수행할 동작
 	private void Touching(Vector2 pos)
 	{
-		float z;
-		if (pos.x == -1 || pos.x == 1)
+		if (isMoving)
 		{
-			z = 49f;
-		}
-		else
-		{
-			z = 50f;
-		}
+			// x좌표 1씩 마다 지그재그로 z좌표가 변경
+			float z;
+			if (pos.x == -1 || pos.x == 1)
+			{
+				z = 49f;
+			}
+			else
+			{
+				z = 50f;
+			}
 
-		transform.position = new Vector3(pos.x, pos.y, z);
-
+			UpdatePositionWithChild(new Vector3(pos.x, pos.y, z)); 
+		}
 	}
 
-	private void TouchReleased(CubeController target)
+	// 모든 Child와 함께 이동
+	private void UpdatePositionWithChild(Vector3 pos)
 	{
-		if (target == this)
+        if (Child != null)
+        {
+			Child.UpdatePositionWithChild(pos + new Vector3(0, 0.75f, 0)); // Child 재귀 (큐브의 높이인 0.75씩 올려서 함께 이동)
+        }
+		transform.position = pos;
+    }
+
+	// 해당 큐브가 놓아질 때 수행할 동작
+	private void TouchReleased()
+	{
+		if (isMoving)
 		{
-			transform.GetComponent<Rigidbody>().isKinematic = false;
-			InputManager.Instance.OnTouching -= Touching;
+			// 레이캐스팅을 통해 현재 큐브의 바닥에 다른 큐브나 루트가 있는지 확인
+			RaycastHit hit;
+			isMoving = false;
+
+			int prevX = Mathf.RoundToInt(prevPos.x); // 이전 x좌표
+			int currentX = Mathf.RoundToInt(transform.position.x); // 현재 x좌표
+
+			Debug.DrawRay(transform.position, Vector3.down * 10f, Color.red, 4f);
+			if (Physics.Raycast(transform.position, Vector3.down, out hit))
+			{
+				if (hit.collider.CompareTag("Cube")) // 큐브면 
+				{
+					if (hit.collider.gameObject.name.Equals("Cube"))
+					{
+						UpdatePositionWithChild(hit.collider.transform.position + new Vector3(0, 0.75f, 0));
+						CubeController newParent = hit.collider.transform.GetComponent<CubeController>();
+						newParent.Child = this;
+						Parent = newParent;
+					}
+					else // 큐브가 아니면 (Root)
+					{				
+						UpdatePositionWithChild(new Vector3(transform.position.x, -12.625f ,transform.position.z));
+						Parent = null;
+					}
+					
+					// GameManager Cubes 갱신
+					GameManager.Instance.UpdateLine(prevX + 2);
+					GameManager.Instance.UpdateLine(currentX + 2);
+
+					return;
+				}
+			}
+
+			// 잘못된 위치에 둔 경우 다시 원위치
+			UpdatePositionWithChild(prevPos);
+			GameManager.Instance.UpdateLine(prevX + 2);
 		}
 	}
 }
--- Jelitaire/Assets/Scripts/Managers/GameManager.cs
+++ Jelitaire/Assets/Scripts/Managers/GameManager.cs
@@ -1,17 +1,19 @@
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
 	[SerializeField]
-	private GameObject Board;
+	private GameObject Board; // 큐브를 올릴 받침대 오브젝트 그룹 (Root 접근에 필요)
 
 	[SerializeField]
-	public Color32[] CubeColors;
+	public Color32[] CubeColors; // 큐브 타입에 따른 색상표
 
-	public enum GameState
+	// 게임 진행 State 패턴
+	public enum GameState 
 	{
 		Init,
 		Game,
@@ -19,16 +21,24 @@ public class GameManager : Singleton<GameManager>
 		Hide
 	}
 
-	private int rootCount = 5;
-	private CubeController[] roots;
-
 	public GameState State;
+
+	private int rootCount = 5; // 큐브를 올릴 수 있는 라인(Root)의 개수
+	public List<CubeController>[] Cubes; // 모든 큐브의 정보를 담고있는 List의 배열
 
 	protected override void Awake()
     {
 		base.Awake();
-		roots = new CubeController[rootCount];
+
+		// Cubes 및 게임 상태 초기화
+		Cubes = new List<CubeController>[rootCount];
+		for (int i = 0; i < rootCount; i++)
+		{
+			Cubes[i] = new List<CubeController>();
+		}
+
 		State = GameState.Hide;
+
 		Application.targetFrameRate = 120; // 게임의 프레임을 선언
 	}
 
@@ -53,6 +63,7 @@ public class GameManager : Singleton<GameManager>
 		}
 	}
 
+	// 큐브 새 줄 추가 버튼 이벤트
 	public void OnAddButtonClicked()
 	{
 		StartCoroutine(AddNewCubes());
@@ -77,6 +88,7 @@ public class GameManager : Singleton<GameManager>
 		Board.SetActive(false);
 	}
 
+	// 새 큐브 줄 추가 로직
 	private IEnumerator AddNewCubes()
 	{
 		yield return new WaitForSeconds(0.5f);
@@ -95,40 +107,61 @@ public class GameManager : Singleton<GameManager>
 			}
 
 			// 새로 만든 큐브의 초기 위치를 지정
-			newCube.transform.position = new Vector3(i - 2, -5, z);
+			newCube.transform.position = new Vector3(i - 2, -12.625f + 0.75f * (Cubes[i].Count), z);
 
 			// TODO : 새로 만든 큐브의 값들을 지정(타입, 숫자 등)
 			newCubeController.SetValue(Random.Range(1, 6), Random.Range(0, 3));
 
 			// 라인의 Root 위치에 자식 오브젝트가 이미 있으면 제일 막내로 넣음
-			if (roots[i] != null)
+			int CubeCountInLine = Cubes[i].Count;
+			if (CubeCountInLine > 0)
 			{
-				// LinkedList와 유사하게 Child와 Parent 형태로 연결되어있는 Cube 객체를 순회
-				CubeController current = roots[i].GetComponent<CubeController>(); // Root의 자식 큐브
-				while (current.Child != null)
-				{
-					current = current.Child;
-				}
+				Cubes[i][CubeCountInLine - 1].Child = newCubeController;
+				newCubeController.Parent = Cubes[i][CubeCountInLine - 1];
+			}
 
-				// current가 계층 구조의 막내 큐브를 가리키면 여기에 Cube를 연결
-				current.Child = newCubeController;
-				newCubeController.Parent = current;
-			}
-			else
-			{
-				roots[i] = newCubeController;
-			}
+			Cubes[i].Add(newCubeController);
 
 			newCube.transform.SetParent(Board.transform);
 		}
 	}
 
+	// 모든 큐브를 삭제
 	private IEnumerator RemoveAllCubes()
 	{
 		yield return null;
 		for (int i = 0; i < rootCount; i++)
 		{
-			roots[i].DestroyCube();
+			if (Cubes[i].Count > 0)
+			{
+				Cubes[i][0].DestroyCube();
+			}
+
+			Cubes[i] = new List<CubeController>(); // Cubes도 비워줌
+		}
+	}
+
+	// 해당 Root에 올려진 모든 큐브의 정보를 업데이트
+	public void UpdateLine(int i)
+	{
+		// Board의 Root 오브젝트에서 레이캐스팅하여 최초 큐브를 찾음
+		// Child로 링크되어 있으므로 링크드리스트처럼 순회 접근
+		Transform root = Board.transform.GetChild(i);
+		RaycastHit hit;
+		CubeController current;
+		Cubes[i] = new List<CubeController>();
+		if (Physics.Raycast(root.transform.position, Vector3.up, out hit))
+		{
+			if (hit.collider.CompareTag("Cube") && hit.collider.gameObject.name.Equals("Cube"))
+			{
+				current = hit.collider.transform.GetComponent<CubeController>();
+
+				while (current != null)
+				{
+					Cubes[i].Add(current);
+					current = current.Child;
+				}
+			}
 		}
 	}
 }

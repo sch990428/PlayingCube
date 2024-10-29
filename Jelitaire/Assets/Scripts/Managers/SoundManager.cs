9db1bb472c75a1686db176367c0901fe2bd7494e
sch990428 <990428sch@naver.com>
2024-10-29 17:54:50 +0900

    SoundManager 추가
    SoundManager 추가

---
+++ Jelitaire/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+	[SerializeField]
+	private List<AudioClip> audioClips; // 오디오 클립
+
+	private AudioSource audioSource;
+
+	// 게임 사운드 요소 (순서 주의)
+	public enum GameSound
+	{
+		Init,
+		Click,
+		Drop,
+		AddLine,
+		Pop,
+		GameOver,
+	}
+
+	private Dictionary<GameSound, AudioClip> audioClipDict;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		audioSource = GetComponent<AudioSource>();
+
+		// 인스펙터로 할당된 사운드클립들을 audioClipDict에 Add
+		audioClipDict = new Dictionary<GameSound, AudioClip>();
+
+		for (int i = 0; i < audioClips.Count; i++)
+		{
+			audioClipDict.Add((GameSound)i, audioClips[i]);
+		}
+	}
+
+	// 사운드 재생
+	public void PlaySound(GameSound type)
+	{
+		audioSource.PlayOneShot(audioClipDict[type]);
+	}
+}
